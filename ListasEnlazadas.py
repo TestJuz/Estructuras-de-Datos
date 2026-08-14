9f99029278ab24dc735829d433f578c52efea5d2
ISAAC HERRERA ALFARO <isaac.herrera.alfaro@est.una.ac.cr>
2026-08-14 08:17:22 -0600

    ListasDoblemente Enlazadas

---
+++ ListasEnlazadas.py
@@ -0,0 +1,175 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+        self.anterior = None
+
+class ListaDoblementeEnlazada:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamanio = 0
+
+    def estaVacia(self):
+        return self.cabeza is None
+
+    def AgregarAlInicio(self,valor):
+        nuevo_nodo = Nodo(valor)
+        if self.estaVacia():
+            self.cabeza = nuevo_nodo
+            self.cola = nuevo_nodo
+        else:
+            nuevo_nodo.siguiente = self.cabeza
+            self.cabeza.anterior = nuevo_nodo
+            self.cabeza = nuevo_nodo
+        self.tamanio += 1
+
+    def RecorrerAdelante(self):
+        actual = self.cabeza
+        while actual is not None:
+            print(actual.valor, end="  ")
+            actual = actual.siguiente
+        print()
+
+    def RecorrerAtras(self):
+        actual = self.cola
+        while actual is not None:
+            print(actual.valor, end="  ")
+            actual = actual.anterior
+    def Buscar(self,valor):
+        actual = self.cabeza
+        posicion = 0
+        while actual:
+            if actual.valor == valor:
+                return posicion
+            actual = actual.siguiente
+            posicion += 1
+        return -1 # este elemento nunca estuvo en la lista doblemente enlazada
+
+    def tamanio_lista(self):
+        return self.tamanio
+
+    def EliminarAlFinal(self):
+        if self.estaVacia():
+            print("La lista está vacía. No se puede eliminar ningún elemento.")
+            return None
+        valor_eliminado = self.cola.valor
+        if self.cabeza == self.cola:  # Solo hay un nodo en la lista
+            self.cabeza = None
+            self.cola = None
+        else:
+            self.cola = self.cola.anterior
+            self.cola.siguiente = None
+        self.tamanio -= 1
+        return valor_eliminado
+
+    def insertar_final(self,valor):
+        nuevo_nodo = Nodo (valor)
+        if self.estaVacia():
+            self.cabeza = nuevo_nodo
+            self.cola = nuevo_nodo
+        else:
+            nuevo_nodo.anterior = self.cola
+            self.cola.siguiente = nuevo_nodo
+            self.cola = nuevo_nodo
+        self.tamanio += 1 
+
+    def insertar_medio(self,valor,posicion):
+        if posicion < 0 or posicion > self.tamanio:
+         print("Posicion Invalida.")
+         return 
+
+        if posicion == 0:
+            self.AgregarAlInicio(valor)
+            return  
+
+        if posicion == self.tamanio:
+            self.insertar_final(valor)
+            return
+
+        nuevo_nodo = Nodo(valor)
+
+        actual = self.cabeza
+
+        for i in range(posicion ):
+            actual = actual.siguiente
+
+        anterior = actual.anterior
+
+        nuevo_nodo.anterior = anterior
+        nuevo_nodo.siguiente = actual
+
+        anterior.siguiente = nuevo_nodo
+        actual.anterior = nuevo_nodo
+
+        self.tamanio +=1
+
+    def eliminar_inicio(self):
+        if self.estaVacia():
+            print("No existe algun elemento al cual eliminar")
+            return None
+
+        valor_eliminado = self.cabeza.valor
+        if self.cabeza == self.cola:
+            self.cabeza = None
+            self.cola = None
+        else:
+            self.cabeza = self.cabeza.siguiente
+            self.cabeza.anterior = None
+
+        self.tamanio -=1
+
+        return valor_eliminado
+
+    def eliminarMedio(self,posicion):
+        if self.estaVacia():
+            print("No existe algun elemento al cual eliminar")
+            return None
+
+        if posicion < 0 or posicion >= self.tamanio:
+            print ("Posicion Invalida.")
+            return None
+
+        if posicion == 0:
+            return self.eliminar_inicio()
+
+        if posicion == self.tamanio - 1:
+            return self.EliminarAlFinal()
+
+        actual = self.cabeza
+
+        for i in range(posicion):
+            actual = actual.siguiente
+
+        valor_eliminado = actual.valor
+        anterior = actual.anterior
+        siguiente = actual.siguiente
+
+        anterior.siguiente = siguiente
+        siguiente.anterior = anterior 
+
+        self.tamanio -= 1
+
+        return valor_eliminado
+    
+
+
+if __name__ == "__main__":
+   listaD = ListaDoblementeEnlazada()
+
+  
+
+
+
+   listaD.insertar_final(20)
+   listaD.insertar_medio(25,1)
+   listaD.insertar_final(15)
+   listaD.RecorrerAdelante()
+   print("Ahora usando los metodos de eliminar: ")
+
+   listaD.eliminarMedio(1)
+   listaD.RecorrerAdelante()
+
+   listaD.eliminar_inicio()
+   listaD.RecorrerAdelante()
+
